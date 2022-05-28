e9d73b9e5b899aea069f919e74d044d1ffd43621
Filip <filip.knias@o2.pl>
2022-05-28 20:29:36 +0200

    react router setup and mui installation

--- src/App.js
+++
@@ -1,7 +0,0 @@
-const App = () => {
-  return (
-    <div>React</div>
-  );
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Dashboard from './pages/Dashboard';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Dashboard />} />
+      </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
